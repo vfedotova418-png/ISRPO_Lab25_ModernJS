f5d37912d5dae4ce46d7c455d0c2745fe8ca9192
vfedotova418-png <vfedotova418@gmail.com>
2026-03-28 16:21:06 +0300

    More validation added

--- converter/main.js
+++ converter/main.js
@@ -4,14 +4,19 @@ const result = document.getElementById("result");
 convertBtn.addEventListener("click", () => {
     let amount = document.getElementById("amount").value;
     let currency = document.getElementById("currency").value;
-    if (amount === "" || amount === null || amount === "0") {
-        result.textContent = "Ошибка: Некорректная сумма"
-    } else {
-        if (currency === "EUR") {
-            result.textContent = `Итог: ${(amount * 0.0107).toFixed(4)} ${currency}`;
-        }
-        if (currency === "USD") {
-            result.textContent = `Итог: ${(amount * 0.0123).toFixed(4)} ${currency}`;
-        }
+    if (amount === "" || amount === null) {
+        result.textContent = "Ошибка: Введите число";
+        return;
+    }
+    amount = parseFloat(amount);
+    if (isNaN(amount) || amount <= 0) {
+        result.textContent = "Ошибка: Некорректный ввод";
+        return;
+    }
+    if (currency === "EUR") {
+        result.textContent = `Итог: ${(amount * 0.0107).toFixed(4)} ${currency}`;
+    }
+    if (currency === "USD") {
+        result.textContent = `Итог: ${(amount * 0.0123).toFixed(4)} ${currency}`;
     }
 });
